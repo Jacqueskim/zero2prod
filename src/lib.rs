99bb35e7411bf2ffc0b888951671e89fadd1df14
Jacques Kim <skim945@umail.ucsb.edu>
2025-05-09 15:22:04 -0700

    post and get working

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,30 @@
 use actix_web::{web, App, HttpResponse, HttpServer};
 use actix_web::dev::Server;
-
+use std::net::TcpListener;
 
 async fn health_check() -> HttpResponse {
     HttpResponse::Ok().finish()
 }
+#[derive (serde::Deserialize)]
+struct FormData {
+    email: String,
+    name: String
+}
 
-pub fn run() -> Result<Server, std::io::Error> {
+async fn subscribe(_form: web::Form<FormData>) -> HttpResponse {
+    HttpResponse::Ok().finish()
+}
+
+pub fn run(listener:TcpListener) -> Result<Server, std::io::Error> {
     let server = HttpServer::new(|| {
         App::new()
             // .route("/", web::get().to(greet))
             // .route("/{name}", web::get().to(greet))
             .route("/health_check", web::get().to(health_check))
+            .route("/subscriptions", web::post().to(subscribe))
     })
-    .bind("127.0.0.1:8080")?
+    .listen(listener)?
     .run();
     Ok(server)
 }
+
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
 use zero2prod::run;
-
+use std::net::TcpListener;
 #[tokio::main]
 async fn main() ->Result<(), std::io::Error> {
     
-    run()?.await
+    let listener = TcpListener::bind("127.0.0.1:0")?;
+    run(listener)?.await
+    
     
 }

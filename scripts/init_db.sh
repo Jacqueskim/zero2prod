739bc295f5c437bd417e1cc5f2e421b752509566
Jacques Kim <skim945@umail.ucsb.edu>
2025-05-15 15:38:26 -0700

    adding database

---
+++ scripts/init_db.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+set -x
+set -eo pipefail
+
+if ! [ -x "$(command -v psql)" ]; then
+    echo >&2 "Error: psql is not installed. Please install PostgreSQL client."
+    exit 1
+fi
+
+if ! [ -x "$(command -v sqlx)" ]; then
+    echo >&2 "Error: sqlx is not installed. Please install sqlx."
+    echo >&2 "Use:"
+    echo >&2 "  cargo install --version='~0.7' sqlx-cli \\"
+    echo >&2 "  --no-default-features --features rustls,postgres"
+    exit 1
+fi
+DB_USER="${POSTGRES_USER:=postgres}"
+DB_PASSWORD="${POSTGRES_PASSWORD:=password}"
+DB_NAME="${POSTGRES_DB:=newsletter}"
+DB_PORT="${POSTGRES_PORT:=5432}"
+DB_HOST="${POSTGRES_HOST:=localhost}"
+
+docker run \
+    -e POSTGRES_USER=${DB_USER} \
+    -e POSTGRES_PASSWORD=${DB_PASSWORD} \
+    -e POSTGRES_DB=${DB_NAME} \
+    -p "${DB_PORT}":5432 \
+    -d postgres \
+    postgres -N 1000
+
+export PGPASSWORD=${DB_PASSWORD}
+until psql -h "{DB_HOST}" -U "${DB_USER}" -p "${DB_PORT}" -d "postgres" -c '\q'; do
+    >&2 echo "postgres os still unavailable - sleeping"
+    sleep 1
+done
+
+>&2 echo "postgres is up and running on port ${DB_PORT}"
+
+DATABASE_URL=postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}
+export DATABASE_URL
+sqlx database create

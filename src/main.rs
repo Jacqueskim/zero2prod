18f7f45700f92f12f7f2d7ae983dbd146eb5e27a
Jacques Kim <skim945@umail.ucsb.edu>
2025-07-08 14:31:32 -0700

    chose no cloud

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use zero2prod::startup::run;
 use std::net::TcpListener;
 use zero2prod::configuration::get_configuration;
 use sqlx::PgPool;
+use sqlx::postgres::PgPoolOptions;
 use zero2prod::telemetry::{get_subscriber, init_subscriber};
 use secrecy::ExposeSecret;
 #[tokio::main]
@@ -11,11 +12,12 @@ async fn main() ->Result<(), std::io::Error> {
     
    
     let configuration = get_configuration().expect("Failed to read configuratrion.");
-    let connection = PgPool::connect_lazy(&configuration.database.connection_string().expose_secret())
-        .expect("Failed to connect to Postgres.");
+    let connection_pool = PgPoolOptions::new()
+    .connect_lazy_with(configuration.database.with_db());
+        // .expect("Failed to connect to Postgres.");
     let address = format!("{}:{}", configuration.application.host, configuration.application.port);
     let listener = TcpListener::bind(address)?;
-    run(listener, connection)?.await?;
+    run(listener, connection_pool)?.await?;
     
     Ok(())
 }

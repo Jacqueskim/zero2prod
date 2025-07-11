ea3ea385f6560957fd06c08a6c16233f9bf3e93a
Jacques Kim <skim945@umail.ucsb.edu>
2025-07-11 14:47:32 -0700

    7.2

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use std::net::TcpListener;
 use zero2prod::configuration::get_configuration;
 use sqlx::postgres::PgPoolOptions;
 use zero2prod::telemetry::{get_subscriber, init_subscriber};
+use zero2prod::email_client::EmailClient;
 #[tokio::main]
 async fn main() ->Result<(), std::io::Error> {
     let subscriber = get_subscriber("zero2prod".into(), "info".into(), std::io::stdout);
@@ -12,10 +13,16 @@ async fn main() ->Result<(), std::io::Error> {
     let configuration = get_configuration().expect("Failed to read configuratrion.");
     let connection_pool = PgPoolOptions::new()
     .connect_lazy_with(configuration.database.with_db());
-        // .expect("Failed to connect to Postgres.");
+
+    let sender_email= configuration.email_client.sender()
+        .expect("Invalid sender email address.");
+    let timeout = configuration.email_client.timeout();
+    let email_client = EmailClient::new(configuration.email_client.base_url, sender_email,
+        configuration.email_client.authorization_token,
+        timeout);
     let address = format!("{}:{}", configuration.application.host, configuration.application.port);
     let listener = TcpListener::bind(address)?;
-    run(listener, connection_pool)?.await?;
+    run(listener, connection_pool,email_client)?.await?;
     
     Ok(())
 }

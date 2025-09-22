b86a016bfe863a1d8f71f417d11502ed8a53567c
Jacques Kim <skim945@umail.ucsb.edu>
2025-09-22 16:34:15 -0700

    finished 10.2

--- tests/api/helpers.rs
+++ tests/api/helpers.rs
@@ -6,6 +6,9 @@ use zero2prod::startup::{get_connection_pool};
 use zero2prod::startup::Application;
 use zero2prod::telemetry::{get_subscriber,init_subscriber};
 use wiremock::MockServer;
+use sha3::Digest;
+use argon2::password_hash::SaltString;
+use argon2::{Argon2, PasswordHasher};
 
 static TRACING: Lazy<()> = Lazy::new(|| {
     let default_filter_level = "info".to_string();
@@ -30,7 +33,8 @@ pub struct TestApp {
     pub address: String,
     pub db_pool: PgPool,
     pub email_server: MockServer,
-    pub port:u16
+    pub port:u16,
+    test_user: TestUser
 }
 
 impl  TestApp{
@@ -39,6 +43,15 @@ impl  TestApp{
             .post(&format!("{}/subscriptions", &self.address))
             .header("Content-Type", "application/x-www-form-urlencoded")
             .body(body)
+            .send()
+            .await
+            .expect("Failed to execute request.")
+    }
+    pub async fn post_newsletters(&self, body:serde_json::Value)->reqwest::Response{
+        reqwest::Client::new()
+            .post(&format!("{}/newsletters", &self.address))
+            .basic_auth(&self.test_user.username, Some(&self.test_user.password))
+            .json(&body)
             .send()
             .await
             .expect("Failed to execute request.")
@@ -71,6 +84,8 @@ impl  TestApp{
             plain_text,
         }
     }
+
+    
 }
 pub async fn spawn_app() -> TestApp {
     Lazy::force(&TRACING);
@@ -90,12 +105,15 @@ pub async fn spawn_app() -> TestApp {
     let application_port = application.port();
     let address = format!("http://127.0.0.1:{}",application.port());
     let _ = tokio::spawn(application.run_until_stopped());
-    TestApp{
+    let test_app =TestApp{
         address,
         db_pool: get_connection_pool(&configuration.database),
         email_server,
         port:application_port,
-    }
+        test_user:TestUser::generate()
+    };
+    test_app.test_user.store(&test_app.db_pool).await;
+    test_app
         
 }
 async fn configure_database(config: &DatabaseSettings) ->PgPool {
@@ -117,4 +135,43 @@ async fn configure_database(config: &DatabaseSettings) ->PgPool {
         .await
         .expect("Failed to migrate the database");
     connection_pool
+}
+
+
+pub struct TestUser {
+    pub user_id : Uuid,
+    pub username: String,
+    pub password: String
+}
+
+impl TestUser {
+    pub fn generate() ->Self {
+        Self {
+            user_id: Uuid::new_v4(),
+            username: Uuid::new_v4().to_string(),
+            password: Uuid::new_v4().to_string(),
+        }
+    }
+
+    async fn store(&self, pool: &PgPool){
+        let salt = SaltString::generate(&mut rand::thread_rng());
+        let password_hash = Argon2::new(
+            Algorithm::Argon2id,
+            Version::V0x13,
+            Params::new(15000,2,1,None).unwrap(),
+        )
+            .hash_password(self.password.as_bytes(), &salt)
+            .unwrap()
+            .to_string();
+        sqlx::query!(
+            "INSERT INTO users (user_id, username, password_hash)
+            VALUES ($1, $2, $3)",
+            self.user_id,
+            self.username,
+            password_hash,
+        )
+        .execute(pool)
+        .await
+        .expect("Failed to store test user.");
+    }
 }
--- tests/api/newsletter.rs
+++ tests/api/newsletter.rs
@@ -3,7 +3,7 @@ use core::error;
 use crate::helpers::{spawn_app,ConfirmationLinks, TestApp};
 use wiremock::matchers::{any, method, path};
 use wiremock::{Mock, ResponseTemplate};
-
+use uuid::Uuid;
 
 #[tokio::test]
 async fn newletters_are_not_delivered_to_unconfirmed_subscribers(){
@@ -115,17 +115,74 @@ async fn newsletters_returns_400_for_invalid_data(){
     }
 }
 
-impl TestApp{
-    pub async fn post_newsletters(
-        &self,
-        body:serde_json::Value)->reqwest::Response{
-            reqwest::Client::new()
-                .post(&format!("{}/newsletters", &self.address))
-                .json(&body)
-                .send()
-                .await
-                .expect("Failed to execute request.")
-        }
-    
+
+
+#[tokio::test]
+async fn requests_missing_authorization_are_rejected(){
+    let app = spawn_app().await;
+    let response = reqwest::Client::new()
+        .post(&format!("{}/newsletters", &app.address))
+        .json(&serde_json::json!({
+            "title": "Newsletter title",
+            "content": {
+                "text": "Newsletter body as plain text",
+                "html": "<p>Newsletter body as HTML </p>"
+            }
+        }))
+        .send()
+        .await.expect("Failed to execute request.");
+
+    assert_eq!(401, response.status().as_u16());
+    assert_eq!(r#"Basic realm="publish""#, response.headers()["WWW-Authenticate"]);
 }
 
+#[tokio::test]
+async fn non_existing_user_is_rejected(){
+    let app = spawn_app().await;
+
+    let username = Uuid::new_v4().to_string();
+    let password= Uuid::new_v4().to_string();
+
+    let response = reqwest::Client::new()
+        .post(&format!("{}/newsletters", &app.address))
+        .basic_auth(username, Some(password))
+        .json(&serde_json::json!({
+            "title": "Newsletter title",
+            "content": {
+                "text": "Newsletter body as plain text",
+                "html": "<p>Newsletter body as HTML </p>"
+            }
+        }))
+        .send()
+        .await
+        .expect("Failed to execute request.");
+
+    assert_eq!(401, response.status().as_u16());
+    assert_eq!(r#"Basic realm="publish""#,
+    response.headers()["WWW-Authenticate"]);
+}
+
+#[tokio::test]
+async fn invalid_password_is_rejected(){
+    let app = spawn_app().await;
+    let username = &app.test_user.username;
+    let password = Uuid::new_v4().to_string();
+
+    let response = reqwest::Client::new()
+        .post(&format!("{}/newsletters", &app.address))
+        .basic_auth(username, Some(password))
+        .json(&serde_json::json!({
+            "title": "Newsletter title",
+            "content": {
+                "text": "Newsletter body as plain text",
+                "html": "<p>Newsletter body as HTML </p>"
+            }
+        }))
+        .send()
+        .await
+        .expect("Failed to execute request.");
+
+    assert_eq!(401, response.status().as_u16());
+    assert_eq!(r#"Basic realm="publish""#,
+    response.headers()["WWW-Authenticate"]);
+}
